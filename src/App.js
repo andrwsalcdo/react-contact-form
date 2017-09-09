80a76fb7c56d9e3a8414d5bd846d79aa9870539a
Andrew Salcedo <asalcedo07@gmail.com>
2017-09-09 01:21:42 -0400

    Add: styled-components, Views/main to hold all the app's components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
+// inject global styles from styled-components. border-sizing, body. 
+import { injectGlobal } from 'styled-components';
+// holds all components
+import Main from './views/Main';
+
+injectGlobal`
+  * {
+    box-sizing: border-box; 
+  }
+  body { 
+    line-height: 1.6; 
+    font-family: Tahoma, Geneva, Verdana, sans-serif; 
+    padding: 1em; 
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Main />
       </div>
     );
   }
---
+++ src/views/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+//import all child components
+import Title from '../components/Title'
+
+
+class Main extends Component {
+    render () {
+        return (
+            <div>y
+                <Title text="Title" />
+            </div>
+        )
+    }
+}
+
+export default Main; 
