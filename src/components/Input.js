448535daf717b3513545a335b35842f3b7a40b4f
Andrew Salcedo <asalcedo07@gmail.com>
2017-09-13 14:45:10 -0400

    Add: Input & Textarea components & styling.

---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    border: 1px solid #E6343B; 
+    padding: 1em; 
+    width: 100%; 
+`; 
+
+export default Input 
--- src/views/ContactForm.js
+++ src/views/ContactForm.js
@@ -4,6 +4,9 @@ import React from 'react';
 import Form from '../components/Form'
 import Label from '../components/Label'
 import StyledButton from '../components/Button'
+import Input from '../components/Input'
+import Textarea from '../components/Textarea'
+
 
 
 const ContactForm = () => (
@@ -11,15 +14,15 @@ const ContactForm = () => (
         <h3>Email Us</h3>
         <Form>
             <Label>Name</Label>
-            <input type="text" name="name" />
+            <Input type="text" name="name" />
             <Label>Company</Label>
-            <input type="text" name="company" />
+            <Input type="text" name="company" />
             <Label>Email Address</Label>
-            <input type="email" name="email" />
+            <Input type="email" name="email" />
             <Label>Phone Number</Label>
-            <input type="text" name="phone" />
+            <Input type="text" name="phone" />
             <Label>Message</Label>
-            <textarea name="message" rows="5"></textarea>
+            <Textarea name="message" rows="5"></Textarea>
             <StyledButton>Submit</StyledButton>
         </Form>
     </div>
