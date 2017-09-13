86e09baa93f494aabd7897e9cdb2e1c06427765c
Andrew Salcedo <asalcedo07@gmail.com>
2017-09-13 16:04:23 -0400

    refactor: form wasn't displaying grid. Add: <WrapperGrid /> around form elements to ensure <Form /> style. Basic mobile first design complete. todo: refactor out components, media queries.

--- src/views/ContactForm.js
+++ src/views/ContactForm.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import { css } from 'styled-components'; 
 
 //import components
 import Form from '../components/Form'
@@ -7,23 +9,40 @@ import StyledButton from '../components/Button'
 import Input from '../components/Input'
 import Textarea from '../components/Textarea'
 
+const WrapperGrid = styled.div`
+    ${props => props.full && css`
+        grid-column: 1 / 3;
+    `}
+`;
 
 
 const ContactForm = () => (
     <div>
         <h3>Email Us</h3>
         <Form>
-            <Label>Name</Label>
-            <Input type="text" name="name" />
-            <Label>Company</Label>
-            <Input type="text" name="company" />
-            <Label>Email Address</Label>
-            <Input type="email" name="email" />
-            <Label>Phone Number</Label>
-            <Input type="text" name="phone" />
-            <Label>Message</Label>
-            <Textarea name="message" rows="5"></Textarea>
-            <StyledButton>Submit</StyledButton>
+            <WrapperGrid>
+                <Label>Name</Label>
+                <Input type="text" name="name" />
+            </WrapperGrid>
+            <WrapperGrid>
+                <Label>Company</Label>
+                <Input type="text" name="company" />
+            </WrapperGrid>
+            <WrapperGrid>
+                <Label>Email Address</Label>
+                <Input type="email" name="email" />
+            </WrapperGrid>
+            <WrapperGrid>
+                <Label>Phone Number</Label>
+                <Input type="text" name="phone" />
+            </WrapperGrid>
+            <WrapperGrid full>
+                <Label>Message</Label>
+                <Textarea name="message" rows="5"></Textarea>
+            </WrapperGrid>
+            <WrapperGrid full>
+                <StyledButton>Submit</StyledButton>
+            </WrapperGrid>
         </Form>
     </div>
 )
